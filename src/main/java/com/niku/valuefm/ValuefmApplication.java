82cedbb5cc588005ff9d33ff45b94f3fd193e7cd
Surendra <surendra.khattri@hotmail.com>
2018-07-02 12:56:59 +1000

    Initalizing Project

---
+++ src/main/java/com/niku/valuefm/ValuefmApplication.java
@@ -0,0 +1,12 @@
+package com.niku.valuefm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuefmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuefmApplication.class, args);
+	}
+}
